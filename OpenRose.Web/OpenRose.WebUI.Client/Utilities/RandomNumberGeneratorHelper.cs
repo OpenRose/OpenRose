e75395dc17c540654b0d2f4de47a86ae6a87f3a0
OpenRose <openroserm@outlook.com>
2025-01-07 18:16:35 +0000

    Added support for creating ItemzType from TreeView (#5)

---
+++ OpenRose.Web/OpenRose.WebUI.Client/Utilities/RandomNumberGeneratorHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace OpenRose.WebUI.Client.Utilities
+{
+    public static class RandomNumberGeneratorHelper
+    {
+        private static readonly Random random = new Random();
+
+        public static int GenerateRandomNumber()
+        {
+            return random.Next(1, 100001);
+        }
+    }
+}
