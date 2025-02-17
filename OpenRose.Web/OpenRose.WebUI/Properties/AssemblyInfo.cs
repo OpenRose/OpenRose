573e0d6d99e42019c09716b4794f4f9884b40b45
OpenRose <openroserm@outlook.com>
2025-02-17 19:00:39 +0000

    Trying to Introduce to AssemblyInfo.cs for Version Details

---
+++ OpenRose.Web/OpenRose.WebUI/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("OpenRose.WebUI")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("OpenRose")]
+[assembly: AssemblyProduct("OpenRose.WebUI")]
+[assembly: AssemblyCopyright("Copyright © OpenRose 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("e5d7fb4c-8f7f-4e23-bf34-ef9e4c3b9b5f")] // New GUID
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyInformationalVersion("0.0.1.0")]
+[assembly: AssemblyMetadata("BranchName", "unknown")]
+[assembly: AssemblyMetadata("CommitSha", "unknown")]
