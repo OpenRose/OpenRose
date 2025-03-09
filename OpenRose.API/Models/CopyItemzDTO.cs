d47254b51c78b404fe96ba71d4eeab25c9313ad6
OpenRose <openroserm@outlook.com>
2025-03-09 13:15:21 +0000

    Introducing CopyItemzDTO

---
+++ OpenRose.API/Models/CopyItemzDTO.cs
@@ -0,0 +1,22 @@
+﻿// OpenRose - Requirements Management
+// Licensed under the Apache License, Version 2.0. 
+// See the LICENSE file or visit https://github.com/OpenRose/OpenRose for more details.
+
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ItemzApp.API.Models
+{
+	/// <summary>
+	/// CopyItemzDTO shall be used for sending in request for creating a copy of 
+	/// Requirements i.e. Itemz including all it's child Itemz.
+	/// </summary>
+	public class CopyItemzDTO
+	{
+		/// <summary>
+		/// Source Itemz's Id for creating a Copy including all it's children.
+		/// </summary>
+		[Required]
+		public Guid ItemzId { get; set; }
+	}
+}
