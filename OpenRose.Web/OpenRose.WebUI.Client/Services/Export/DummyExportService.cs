1962ec5b730490821762eb7fc173a1982f7d8262
OpenRose <openroserm@outlook.com>
2025-06-25 13:31:15 +0100

    Introduced WebUI.Client IExportService
    It is used for obtaining exported file data from the ASP.NET Controller Web API endpoint.

---
+++ OpenRose.Web/OpenRose.WebUI.Client/Services/Export/DummyExportService.cs
@@ -0,0 +1,23 @@
+﻿// OpenRose - Requirements Management
+// Licensed under the Apache License, Version 2.0. 
+// See the LICENSE file or visit https://github.com/OpenRose/OpenRose for more details.
+
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OpenRose.WebUI.Client.Services.Export
+{
+	public class DummyExportService : IExportService
+	{
+
+		/// <inheritdoc/>
+		public async Task<(byte[]? fileContent, string? fileName)> DownloadExportHierarchyAsync(Guid exportRecordId, CancellationToken cancellationToken = default)
+		{
+			throw new InvalidOperationException("OpenRose API base URL is not configured. Please provide a valid URL in the configuration file.");
+		}
+	}
+}
---
+++ OpenRose.Web/OpenRose.WebUI.Client/Services/Export/IExportService.cs
@@ -0,0 +1,23 @@
+﻿// OpenRose - Requirements Management
+// Licensed under the Apache License, Version 2.0. 
+// See the LICENSE file or visit https://github.com/OpenRose/OpenRose for more details.
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OpenRose.WebUI.Client.Services.Export
+{
+	public interface IExportService
+	{
+		/// <summary>
+		/// Downloads the exported hierarchy as a JSON file for a given record ID.
+		/// </summary>
+		/// <param name="exportRecordId">The GUID of the record to export.</param>
+		/// <param name="cancellationToken">Optional cancellation token.</param>
+		/// <returns>
+		/// A tuple containing the file bytes and the suggested filename.
+		/// </returns>
+		Task<(byte[]? fileContent, string? fileName)> DownloadExportHierarchyAsync(Guid exportRecordId, CancellationToken cancellationToken = default);
+	}
+}
