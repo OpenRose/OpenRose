0ccdfb6fcb54baa49cd5ad5b5f0600f846678c1f
OpenRose <openroserm@outlook.com>
2025-07-15 01:27:36 +0100

    Introducing ImportResult and ImportSummaryDTO
    They are used for returning information post completion of successful import

---
+++ OpenRose.API/Models/ImportResult.cs
@@ -0,0 +1,17 @@
+﻿using ItemzApp.API.Models;
+using System;
+using System.Collections.Generic;
+
+namespace ItemzApp.API.Models
+{
+	/// <summary>
+	/// Represents the result of an import operation.
+	/// </summary>
+	public class ImportResult
+	{
+		public bool Success { get; set; }
+		public Guid? ImportedRootId { get; set; }
+		public ImportSummaryDTO? ImportSummary { get; set; }
+		public List<string>? Errors { get; set; }
+	}
+}
---
+++ OpenRose.API/Models/ImportSummaryDTO.cs
@@ -0,0 +1,12 @@
+﻿// OpenRose - Requirements Management
+// Licensed under the Apache License, Version 2.0. 
+// See the LICENSE file or visit https://github.com/OpenRose/OpenRose for more details.
+
+namespace ItemzApp.API.Models
+{
+	public class ImportSummaryDTO
+	{
+		public int TotalCreated { get; set; }
+		public int Depth { get; set; }
+	}
+}
