d7e46e2e7e2b336ccdd8bb6632a4ff558f90538e
OpenRose <openroserm@outlook.com>
2025-12-25 16:23:52 +0000

    DB Migration for adding Trace Label field
    We have two tables in requirements management tools repository which are ItemzJoinItemzTrace and BaselineItemzJoinItemzTrace

---
+++ OpenRose.API/Migrations/20251225152756_AddTraceLabelToItemzJoinItemzTrace.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OpenRose.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTraceLabelToItemzJoinItemzTrace : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TraceLabel",
+                table: "ItemzJoinItemzTrace",
+                type: "nvarchar(32)",
+                maxLength: 32,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TraceLabel",
+                table: "BaselineItemzJoinItemzTrace",
+                type: "nvarchar(32)",
+                maxLength: 32,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TraceLabel",
+                table: "ItemzJoinItemzTrace");
+
+            migrationBuilder.DropColumn(
+                name: "TraceLabel",
+                table: "BaselineItemzJoinItemzTrace");
+        }
+    }
+}
