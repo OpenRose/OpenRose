a1b848a2526869f8ddeba1953b84676a20f06958
OpenRose <openroserm@outlook.com>
2025-12-30 12:50:24 +0000

    Added support for TraceLabel in BaselineTraceabilityComponent and BaselineTraceabilityTreeViewComponent

---
+++ OpenRose.Web/OpenRose.WebUI.Client/SharedModels/ClientSideUIOnlyModel/BaselineTraceComponentViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace OpenRose.WebUI.Client.SharedModels.ClientSideUIOnlyModel
+{
+	public class BaselineTraceComponentViewModel
+	{
+		public Guid Id { get; set; }
+		public string? Name { get; set; }
+		public string? Status { get; set; }
+		public string? Priority { get; set; }
+		public bool isIncluded { get; set; }
+		public string? TraceLabel { get; set; }
+	}
+}
