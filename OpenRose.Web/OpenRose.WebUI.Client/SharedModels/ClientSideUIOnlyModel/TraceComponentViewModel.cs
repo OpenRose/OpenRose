cf73969bb8bfcfddc183eff04e79954dae1bdb74
OpenRose <openroserm@outlook.com>
2025-12-30 12:48:42 +0000

    Added support for TraceLabel in TraceabilityComponent of Requirements

---
+++ OpenRose.Web/OpenRose.WebUI.Client/SharedModels/ClientSideUIOnlyModel/TraceComponentViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace OpenRose.WebUI.Client.SharedModels.ClientSideUIOnlyModel
+{
+	public class TraceComponentViewModel
+	{
+		public Guid Id { get; set; }
+		public string? Name { get; set; }
+		public string? Status { get; set; }
+		public string? Priority { get; set; }
+		public string? TraceLabel { get; set; }
+	}
+
+}
