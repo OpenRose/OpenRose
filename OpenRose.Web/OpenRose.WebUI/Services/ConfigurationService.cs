4bef46a8749b154c018fe85cc1ff23dc69ab4eda
OpenRose <openroserm@outlook.com>
2026-01-17 15:27:02 +0000

    Added OpenRose License information in ConfigurationServices

--- OpenRose.Web/OpenRose.WebUI/Services/ConfigurationService.cs
+++ OpenRose.Web/OpenRose.WebUI/Services/ConfigurationService.cs
@@ -1,4 +1,8 @@
-﻿namespace OpenRose.WebUI.Services
+﻿// OpenRose - Requirements Management
+// Licensed under the Apache License, Version 2.0. 
+// See the LICENSE file or visit https://github.com/OpenRose/OpenRose for more details.
+
+namespace OpenRose.WebUI.Services
 {
     public class ConfigurationService
     {
