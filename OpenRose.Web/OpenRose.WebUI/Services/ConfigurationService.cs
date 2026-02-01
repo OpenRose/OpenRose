756b76a4cd9e40eeca504f1948b3c537ef7ce1aa
OpenRose <openroserm@outlook.com>
2026-02-01 21:05:59 +0000

    Adding Version related new Configuration Properties

--- OpenRose.Web/OpenRose.WebUI/Services/ConfigurationService.cs
+++ OpenRose.Web/OpenRose.WebUI/Services/ConfigurationService.cs
@@ -7,5 +7,12 @@ namespace OpenRose.WebUI.Services
     public class ConfigurationService
     {
         public bool IsOpenRoseAPIConfigured { get; set; }
-    }
+
+		// API and WebUI version information
+		public string? ApiVersion { get; set; }
+		public string? WebUiVersion { get; set; }
+
+		// User-visible message when versions don't match or API cannot be reached
+		public string? ApiVersionMismatchMessage { get; set; }
+	}
 }
