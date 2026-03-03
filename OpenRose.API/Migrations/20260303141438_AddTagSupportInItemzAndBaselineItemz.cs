e00cb5f139145be6f2b9335ef3e5252774855fa2
OpenRose <openroserm@outlook.com>
2026-03-03 14:45:17 +0000

    Adding EF Migration for supporting Tagging in Requirement Items and Baseline Requirement Items

---
+++ OpenRose.API/Migrations/20260303141438_AddTagSupportInItemzAndBaselineItemz.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OpenRose.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTagSupportInItemzAndBaselineItemz : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Tags",
+                table: "Itemzs",
+                type: "NVARCHAR(512)",
+                maxLength: 512,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Tags",
+                table: "BaselineItemz",
+                type: "NVARCHAR(512)",
+                maxLength: 512,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Tags",
+                table: "Itemzs");
+
+            migrationBuilder.DropColumn(
+                name: "Tags",
+                table: "BaselineItemz");
+        }
+    }
+}
