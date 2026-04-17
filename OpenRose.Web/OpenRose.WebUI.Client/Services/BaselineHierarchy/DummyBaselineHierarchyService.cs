2f64a30acb7e076e9fd9198025d2957089a13387
OpenRose <openroserm@outlook.com>
2026-04-17 13:05:26 +0100

    Adding client side service for Baseline Roll-up Estimation in WebUI.Client
    This will be used by Blazor components in the UI

--- OpenRose.Web/OpenRose.WebUI.Client/Services/BaselineHierarchy/DummyBaselineHierarchyService.cs
+++ OpenRose.Web/OpenRose.WebUI.Client/Services/BaselineHierarchy/DummyBaselineHierarchyService.cs
@@ -6,36 +6,42 @@ using OpenRose.WebUI.Client.SharedModels;
 
 namespace OpenRose.WebUI.Client.Services.BaselineHierarchy
 {
-    public class DummyBaselineHierarchyService : IBaselineHierarchyService
-    {
-        public Task<ICollection<NestedBaselineHierarchyIdRecordDetailsDTO>> __Get_All_Children_Baseline_Hierarchy_By_GUID__Async(Guid recordId)
-        {
-            throw new NotImplementedException();
-        }
+	public class DummyBaselineHierarchyService : IBaselineHierarchyService
+	{
+		public Task<ICollection<NestedBaselineHierarchyIdRecordDetailsDTO>> __Get_All_Children_Baseline_Hierarchy_By_GUID__Async(Guid recordId)
+		{
+			throw new NotImplementedException();
+		}
 
-        public Task<int> __Get_All_Children_Baseline_Hierarchy_Count_By_GUID__Async(Guid recordId)
-        {
-            throw new NotImplementedException();
-        }
+		public Task<int> __Get_All_Children_Baseline_Hierarchy_Count_By_GUID__Async(Guid recordId)
+		{
+			throw new NotImplementedException();
+		}
 
-        public Task<ICollection<NestedBaselineHierarchyIdRecordDetailsDTO>> __Get_All_Parents_Baseline_Hierarchy_By_GUID__Async(Guid recordId)
-        {
-            throw new NotImplementedException();
-        }
+		public Task<ICollection<NestedBaselineHierarchyIdRecordDetailsDTO>> __Get_All_Parents_Baseline_Hierarchy_By_GUID__Async(Guid recordId)
+		{
+			throw new NotImplementedException();
+		}
 
-        public Task<BaselineHierarchyIdRecordDetailsDTO> __Get_BaselineHierarchy_Record_Details_By_GUID__Async(Guid recordId)
-        {
-            throw new NotImplementedException();
-        }
+		public Task<BaselineHierarchyIdRecordDetailsDTO> __Get_BaselineHierarchy_Record_Details_By_GUID__Async(Guid recordId)
+		{
+			throw new NotImplementedException();
+		}
 
-        public Task<ICollection<BaselineHierarchyIdRecordDetailsDTO>> __Get_Immediate_Children_Baseline_Hierarchy_By_GUID__Async(Guid recordId)
-        {
-            throw new NotImplementedException();
-        }
+		public Task<ICollection<BaselineHierarchyIdRecordDetailsDTO>> __Get_Immediate_Children_Baseline_Hierarchy_By_GUID__Async(Guid recordId)
+		{
+			throw new NotImplementedException();
+		}
 
-        public Task<bool> __Get_VerifyParentChild_BreakdownStructure__Async(Guid? parentId, Guid? childId)
-        {
-            throw new NotImplementedException();
-        }
-    }
+		public Task<bool> __Get_VerifyParentChild_BreakdownStructure__Async(Guid? parentId, Guid? childId)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<string> __POST_Recalculate_Baseline_RollUpEstimations__Async(Guid baselineHierarchyRecordId)
+		{
+			throw new NotImplementedException();
+		}
+
+	}
 }
