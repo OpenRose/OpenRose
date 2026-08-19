0e810ea948ce4e086592208c14d518d146f3182b
OpenRose <openroserm@outlook.com>
2026-08-19 23:31:08 +0100

    Introducing navigationEvents as custom JavaScript event

---
+++ OpenRose.Web/OpenRose.WebUI/wwwroot/js/navigationEvents.js
@@ -0,0 +1,18 @@
+﻿/*
+ * OpenRose - Requirements Management
+    * Licensed under the Apache License, Version 2.0.
+ * See the LICENSE file or visit https://github.com/OpenRose/OpenRose for more details.
+*/
+
+window.OpenRoseNavigation = {
+    registerNavigationHandler: function (dotNetObj) {
+        window.addEventListener("beforeunload", function (event) {
+            const navEntry = performance.getEntriesByType("navigation")[0];
+            const navType = navEntry ? navEntry.type : "navigate";
+
+            // Send navigation type to .NET
+            dotNetObj.invokeMethodAsync("OnBrowserNavigation", navType);
+        });
+    }
+};
+
