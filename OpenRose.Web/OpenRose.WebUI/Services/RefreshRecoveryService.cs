47554dddf77cd1b09e0f7c78ef5ddd1cb960a9e1
OpenRose <openroserm@outlook.com>
2026-08-21 23:30:16 +0100

    Introducing ShouldClear boolean flag in RefreshRecoveryService

--- OpenRose.Web/OpenRose.WebUI/Services/RefreshRecoveryService.cs
+++ OpenRose.Web/OpenRose.WebUI/Services/RefreshRecoveryService.cs
@@ -2,7 +2,6 @@
 // Licensed under the Apache License, Version 2.0. 
 // See the LICENSE file or visit https://github.com/OpenRose/OpenRose for more details.
 
-
 using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
 
 namespace OpenRose.WebUI.Services
@@ -17,10 +16,25 @@ namespace OpenRose.WebUI.Services
 		private const string KEY_EXPANDED = "RR_EXPANDED_IDS";
 		private const string KEY_SCROLL = "RR_SCROLL_POS";
 
+		// NEW: Flag to indicate deferred clearing
+		public bool ShouldClear { get; private set; } = false;
+
 		public RefreshRecoveryService(ProtectedSessionStorage session)
 		{
 			_session = session;
 		}
+
+		// NEW: Mark that clearing should happen later (MainLayout)
+		public void MarkForClear()
+		{
+			ShouldClear = true;
+		}
+
+		public void DontMarkForClear()
+		{
+			ShouldClear = false;
+		}
+
 
 		// -----------------------------
 		// SAVE STATE
@@ -88,11 +102,20 @@ namespace OpenRose.WebUI.Services
 		// -----------------------------
 		public async Task ClearAllAsync()
 		{
+			// IMPORTANT:
+			// This must NEVER run inside component disposal or navigation events.
+			// otherwise it will reset SignalR connection with server 
+			// which will enforce reloading entire browser page 
+			//
+			// MainLayout will call this safely AFTER render.
+
 			await _session.DeleteAsync(KEY_JSON_FILE);
 			await _session.DeleteAsync(KEY_ROOT_ID);
 			await _session.DeleteAsync(KEY_SELECTED_ID);
 			await _session.DeleteAsync(KEY_EXPANDED);
 			await _session.DeleteAsync(KEY_SCROLL);
+
+			ShouldClear = false; // reset flag
 		}
 	}
 }
